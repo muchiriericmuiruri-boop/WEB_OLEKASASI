87cb6ccd887f8b6c8f4d641bd7a1a6eecb1fb739
muchiriericmuiruri-boop <muchiriericmuiruri@gmail.com>
2025-11-19 15:58:06 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,15 @@
-// Firebase configuration
+// Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyD2HSA-9mS520eGr2d8673r1Eft44-KkJw",
-    authDomain: "olekasasi-mixed-day-secondary.firebaseapp.com",
-    projectId: "olekasasi-mixed-day-secondary",
-    storageBucket: "olekasasi-mixed-day-secondary.appspot.com",
-    messagingSenderId: "1010905296142",
-    appId: "1:1010905296142:web:dc72e1a675247737a3bb5f"
+  apiKey: "AIzaSyD2HSA-9mS520eGr2d8673r1Eft44-KkJw",
+  authDomain: "olekasasi-mixed-day-secondary.firebaseapp.com",
+  projectId: "olekasasi-mixed-day-secondary",
+  storageBucket: "olekasasi-mixed-day-secondary.appspot.com",
+  messagingSenderId: "1010905296142",
+  appId: "1:1010905296142:web:dc72e1a675247737a3bb5f"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
