f27d32e8b54de95e80547f9da5f4207f47ca6a9c
muchiriericmuiruri-boop <muchiriericmuiruri@gmail.com>
2025-11-19 15:44:48 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
+// Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyD2HSA-9mS520eGr2d8673r1Eft44-KkJw",
     authDomain: "olekasasi-mixed-day-secondary.firebaseapp.com",
     projectId: "olekasasi-mixed-day-secondary",
-    storageBucket: "olekasasi-mixed-day-secondary.firebasestorage.app",
+    storageBucket: "olekasasi-mixed-day-secondary.appspot.com",
     messagingSenderId: "1010905296142",
     appId: "1:1010905296142:web:dc72e1a675247737a3bb5f"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
 const auth = firebase.auth();
+const db = firebase.firestore();
 const storage = firebase.storage();
