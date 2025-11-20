43bbc5a7c49fcfc6997dda5356b12e6e7bcc2609
muchiriericmuiruri-boop <muchiriericmuiruri@gmail.com>
2025-11-20 09:22:45 +0300

    Update firebase.js

--- login/firebase.js
+++ login/firebase.js
@@ -1,5 +1,4 @@
-// firebase.js  --- FIREBASE INITIALIZATION (MODULE VERSION)
-
+// firebase.js — FIREBASE INITIALIZATION (MODULE VERSION)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
@@ -9,7 +8,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyD2HSA-9mS520eGr2d8673r1Eft44-KkJw",
     authDomain: "olekasasi-mixed-day-secondary.firebaseapp.com",
     projectId: "olekasasi-mixed-day-secondary",
-    storageBucket: "olekasasi-mixed-day-secondary.firebasestorage.app",
+    storageBucket: "olekasasi-mixed-day-secondary.appspot.com",
     messagingSenderId: "1010905296142",
     appId: "1:1010905296142:web:dc72e1a675247737a3bb5f",
     measurementId: "G-7LB6ZVZ7GC"
@@ -22,4 +21,3 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
