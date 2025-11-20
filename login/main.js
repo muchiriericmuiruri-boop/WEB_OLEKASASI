34a2ca2d727ceaf6791bb6098b18047980d76b92
muchiriericmuiruri-boop <muchiriericmuiruri@gmail.com>
2025-11-20 08:49:05 +0300

    Update main.js

--- login/main.js
+++ login/main.js
@@ -1,1 +1,12 @@
+// Protect pages that require login (optional)
+
+import { auth } from "./firebase.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        // not logged in
+        window.location.href = "../login/login.html";
+    }
+});
 
